3c5fe8f634be8bab6630a1fd8c0bc756ff7ca61e
bradrisse <bradly.risse@gmail.com>
2017-01-31 21:45:48 -0800

    List Build

--- dist/spinner/chasing-dots.js
+++ dist/spinner/chasing-dots.js
@@ -40,24 +40,23 @@ var ChasingDotsComponent = (function () {
     ChasingDotsComponent.prototype.ngOnDestroy = function () {
         this.cancel();
     };
-    __decorate([
-        core_1.Input(), 
-        __metadata('design:type', Number)
-    ], ChasingDotsComponent.prototype, "delay", void 0);
-    __decorate([
-        core_1.Input(), 
-        __metadata('design:type', Boolean), 
-        __metadata('design:paramtypes', [Boolean])
-    ], ChasingDotsComponent.prototype, "isRunning", null);
-    ChasingDotsComponent = __decorate([
-        core_1.Component({
-            selector: 'sk-chasing-dots',
-            styles: ["\n    .chasing-dots-spinner {\n      position: relative;\n      margin: 25px auto;\n      width: 40px;\n      height: 40px;\n    \n      -webkit-animation: sk-rotate 2.0s infinite linear;\n      animation: sk-rotate 2.0s infinite linear;\n    }\n    \n    .dot1,\n    .dot2 {\n      position: absolute;\n      top: 0;\n      display: inline-block;\n      width: 60%;\n      height: 60%;\n      border-radius: 100%;\n      background-color: #333;\n    \n      -webkit-animation: sk-bounce 2.0s infinite ease-in-out;\n      animation: sk-bounce 2.0s infinite ease-in-out;\n    }\n    \n    .dot2 {\n      top: auto;\n      bottom: 0;\n      -webkit-animation-delay: -1.0s;\n      animation-delay: -1.0s;\n    }\n    \n    @-webkit-keyframes sk-rotate {\n      100% {\n        -webkit-transform: rotate(360deg)\n      }\n    }\n    \n    @keyframes sk-rotate {\n      100% {\n        transform: rotate(360deg);\n        -webkit-transform: rotate(360deg)\n      }\n    }\n    \n    @-webkit-keyframes sk-bounce {\n      0%, 100% {\n        -webkit-transform: scale(0.0)\n      }\n      50% {\n        -webkit-transform: scale(1.0)\n      }\n    }\n    \n    @keyframes sk-bounce {\n      0%, 100% {\n        transform: scale(0.0);\n        -webkit-transform: scale(0.0);\n      }\n      50% {\n        transform: scale(1.0);\n        -webkit-transform: scale(1.0);\n      }\n    }\n  "],
-            template: "\n    <div [hidden]=\"!visible\" class=\"chasing-dots-spinner\">\n      <div class=\"dot1\"></div>\n      <div class=\"dot2\"></div>\n    </div>\n  "
-        }), 
-        __metadata('design:paramtypes', [])
-    ], ChasingDotsComponent);
     return ChasingDotsComponent;
 }());
+__decorate([
+    core_1.Input(),
+    __metadata("design:type", Number)
+], ChasingDotsComponent.prototype, "delay", void 0);
+__decorate([
+    core_1.Input(),
+    __metadata("design:type", Boolean),
+    __metadata("design:paramtypes", [Boolean])
+], ChasingDotsComponent.prototype, "isRunning", null);
+ChasingDotsComponent = __decorate([
+    core_1.Component({
+        selector: 'sk-chasing-dots',
+        styles: ["\n    .chasing-dots-spinner {\n      position: relative;\n      margin: 25px auto;\n      width: 40px;\n      height: 40px;\n    \n      -webkit-animation: sk-rotate 2.0s infinite linear;\n      animation: sk-rotate 2.0s infinite linear;\n    }\n    \n    .dot1,\n    .dot2 {\n      position: absolute;\n      top: 0;\n      display: inline-block;\n      width: 60%;\n      height: 60%;\n      border-radius: 100%;\n      background-color: #333;\n    \n      -webkit-animation: sk-bounce 2.0s infinite ease-in-out;\n      animation: sk-bounce 2.0s infinite ease-in-out;\n    }\n    \n    .dot2 {\n      top: auto;\n      bottom: 0;\n      -webkit-animation-delay: -1.0s;\n      animation-delay: -1.0s;\n    }\n    \n    @-webkit-keyframes sk-rotate {\n      100% {\n        -webkit-transform: rotate(360deg)\n      }\n    }\n    \n    @keyframes sk-rotate {\n      100% {\n        transform: rotate(360deg);\n        -webkit-transform: rotate(360deg)\n      }\n    }\n    \n    @-webkit-keyframes sk-bounce {\n      0%, 100% {\n        -webkit-transform: scale(0.0)\n      }\n      50% {\n        -webkit-transform: scale(1.0)\n      }\n    }\n    \n    @keyframes sk-bounce {\n      0%, 100% {\n        transform: scale(0.0);\n        -webkit-transform: scale(0.0);\n      }\n      50% {\n        transform: scale(1.0);\n        -webkit-transform: scale(1.0);\n      }\n    }\n  "],
+        template: "\n    <div [hidden]=\"!visible\" class=\"chasing-dots-spinner\">\n      <div class=\"dot1\"></div>\n      <div class=\"dot2\"></div>\n    </div>\n  "
+    })
+], ChasingDotsComponent);
 exports.ChasingDotsComponent = ChasingDotsComponent;
 //# sourceMappingURL=chasing-dots.js.map
--- dist/spinner/three-bounce.d.ts
+++ dist/spinner/three-bounce.d.ts
@@ -3,6 +3,9 @@ export declare class ThreeBounceComponent implements OnDestroy {
     private visible;
     private timeout;
     delay: number;
+    color: string;
+    size: number;
+    margin: string;
     isRunning: boolean;
     private cancel();
     ngOnDestroy(): any;
